2ac2940031458c5583053f56a85a709f75ad97d3
Zaid <zaidakhalwaya6@gmail.com>
2025-06-20 20:21:40 +0200

    fix nav bar

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleMenu() {
   menu.classList.toggle("show");
 }
 
-// Auto-close menu when any link is clicked
+
 document.querySelectorAll("#myLinks a").forEach(link => {
   link.addEventListener("click", () => {
     document.getElementById("myLinks").classList.remove("show");
