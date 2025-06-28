4fd0bea802d088ed5fbcd57b6c426d1b84d62126
Zaid <zaidakhalwaya6@gmail.com>
2025-06-28 09:13:16 +0200

    i dont like these changes

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ document.querySelectorAll("#myLinks a").forEach(link => {
     document.getElementById("myLinks").classList.remove("show");
   });
 });
+
+  const form = document.getElementById("contactForm");
+  form.addEventListener("submit", function () {
+    alert("✅ Message sent successfully!");
+    form.reset(); 
+  });
