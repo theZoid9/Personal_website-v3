098361e8f5eb654d2ab82e51bdfc5c94ecd80872
Zaid <zaidakhalwaya6@gmail.com>
2025-07-01 15:18:30 +0200

    Fix form

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ document.querySelectorAll("#myLinks a").forEach(link => {
   });
 });
 
+const form = document.getElementById("contactForm");
+const successMsg = document.getElementById("successMsg");
+const errorMsg = document.getElementById("errorMsg");
+
+const actionURL = "https://formspree.io/f/mwpbyyzg"; 
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    successMsg.style.display = "none";
+    errorMsg.style.display = "none";
+
+    const formData = new FormData(form);
+
+    try {
+      const response = await fetch(actionURL, {
+        method: "POST",
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      if (response.ok) {
+        successMsg.style.display = "block";
+        form.reset();
+      } else {
+        errorMsg.style.display = "block";
+      }
+    } catch (error) {
+      errorMsg.style.display = "block";
+      console.error("Form submission error:", error);
+    }
+  });
+
+
